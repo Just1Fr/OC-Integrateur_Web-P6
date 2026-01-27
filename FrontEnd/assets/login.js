12f154f6964f9938e21699be6d5d79754ef402da
Just1Fr <190003579+Just1Fr@users.noreply.github.com>
2026-01-27 18:12:26 +0100

    feat: add the ability to upload new work from a form in the modal

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -4,28 +4,35 @@ if (localStorage.getItem('token')) {
 }
 
 async function tryLogin(email, password) {
-    // Send request to API
-    const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-            "accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        })
-    });
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password
+            })
+        });
 
-    // Handle response from API
-    switch (response.status) {
-        case 200:
+        if (!response.ok) {
+            loginError(response.status);
+        } else {
             const result = await response.json();
             localStorage.setItem('token', result.token);
             localStorage.setItem('userId', result.userId);
             window.location.href = 'index.html';
-            break;
+        }
 
+    }  catch {
+        console.error("Failed to log in");
+    }
+}
+
+function loginError(status) {
+    switch (status) {
         case 401:
         case 404:
             const warning = document.querySelector("#login-form > .input.submit > .warning");
@@ -48,31 +55,31 @@ function isEmailValid(email) {
 }
 
 // Click on log in button
-const submitBtn = document.getElementById("submit");
-submitBtn.addEventListener("click", event => {
-    event.preventDefault();
+document.getElementById("submit")
+    .addEventListener("click", event => {
+        event.preventDefault();
 
-    // Verify if email is valid
-    const email = document.getElementById("email").value;
-    if (!isEmailValid(email)) {
-        document.querySelector("#login-form > .input.email > .warning")
-            .style.display = "block";
-        return
-    } else {
-        document.querySelector("#login-form > .input.email > .warning")
-            .style.display = "none";
-    }
+        // Verify if email is valid
+        const email = document.getElementById("email").value;
+        if (!isEmailValid(email)) {
+            document.querySelector("#login-form > .input.email > .warning")
+                .style.display = "block";
+            return;
+        } else {
+            document.querySelector("#login-form > .input.email > .warning")
+                .style.display = "none";
+        }
 
-    // Verify if password is not empty
-    const password = document.getElementById("password").value;
-    if (!password) {
-        document.querySelector("#login-form > .input.password > .warning")
-            .style.display = "block";
-        return
-    } else {
-        document.querySelector("#login-form > .input.password > .warning")
-            .style.display = "none";
-    }
+        // Verify if password is not empty
+        const password = document.getElementById("password").value;
+        if (!password) {
+            document.querySelector("#login-form > .input.password > .warning")
+                .style.display = "block";
+            return;
+        } else {
+            document.querySelector("#login-form > .input.password > .warning")
+                .style.display = "none";
+        }
 
-    tryLogin(email, password);
-});
+        tryLogin(email, password);
+    });
